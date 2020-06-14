ecd24c12201a1431b5ad2b2ce1481253c612b9dd
klisostom <you@example.com>
2020-06-13 21:28:20 -0300

    Chute o número

--- Projetos/simulador_dado_6.py
+++ Projetos/simulador_dado_6.py
@@ -1,6 +1,10 @@
 import random
 
-print("Você gostaria de jogar o dado? Digite 'sim' ou 'não' sem aspas:")
+"""
+Objetivo: Seu script deve gerar um valor aleatório entre 1 e 6(ou uma faixa que você definir) e permitir que o usuário rode o script quantas vezes quiser.
+"""
+
+print("Você gostaria de jogar o dado? Digite 'sim' ou 'não', sem aspas:")
 resposta = input().lower().strip()
 
 while True:
@@ -11,6 +15,6 @@ while True:
     else:
         print("Informe a resposta esperada: 'sim' ou 'não, sem aspas!")
 
-    print("Você gostaria de jogar o dado? Digite 'sim' ou 'não' sem aspas:")
+    print("Você gostaria de jogar o dado? Digite 'sim' ou 'não', sem aspas:")
     resposta = input().lower().strip()
 
